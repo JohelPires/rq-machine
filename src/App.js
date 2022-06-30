ea0bfdcb3155a7d836cac7665cbc9f324790a407
JohelPires <johelpires@gmail.com>
2022-06-30 15:55:32 -0400

    new quote button is working

--- src/App.js
+++ src/App.js
@@ -5,25 +5,39 @@ function App() {
   // const url = 'https://zenquotes.io/api/quotes'
 
   const [quote, setQuote] = useState({})
+  const [callGetQuote, setCallGetQuote] = useState(false)
 
   const api_url = 'http://api.quotable.io/random'
 
-  useEffect(() => {
-    async function getapi(url) {
-      const response = await fetch(url, { cache: 'no-store' })
-      var data = await response.json()
-      setQuote(data)
-    }
+  // useEffect(() => {
+  //   async function getapi(url) {
+  //     const response = await fetch(url, { cache: 'no-store' })
+  //     var data = await response.json()
+  //     setQuote(data)
+  //   }
+  //   getapi(api_url)
+  // }, [])
 
-    getapi(api_url)
-  }, [])
+  function getQuote() {
+    setCallGetQuote((prev) => !prev)
+  }
+
+  useEffect(() => {
+    fetch(api_url)
+      .then((res) => res.json())
+      .then((data) => {
+        setQuote(data)
+      })
+  }, [callGetQuote])
 
   return (
     <div className='App'>
       <div className='quote-box' id='quote-box'>
         <h1 id='text'>{quote.content}</h1>
         <h3 id='author'>{quote.author}</h3>
-        <button id='new-quote'>New quote</button>
+        <button id='new-quote' onClick={getQuote}>
+          New quote
+        </button>
         <a id='tweet-quote' href=''>
           Tweet Quote
         </a>
--- userStories.md
+++ userStories.md
@@ -12,9 +12,9 @@
 
 [x] User Story #7: On first load, my quote machine displays the random quote's author in the element with id="author".
 
-User Story #8: When the #new-quote button is clicked, my quote machine should fetch a new quote and display it in the #text element.
+[x] User Story #8: When the #new-quote button is clicked, my quote machine should fetch a new quote and display it in the #text element.
 
-User Story #9: My quote machine should fetch the new quote's author when the #new-quote button is clicked and display it in the #author element.
+[x] User Story #9: My quote machine should fetch the new quote's author when the #new-quote button is clicked and display it in the #author element.
 
 User Story #10: I can tweet the current quote by clicking on the #tweet-quote a element. This a element should include the "twitter.com/intent/tweet" path in its href attribute to tweet the current quote.
 
